af1d4a89a591ec36ce7691d1ebb338ea26caacee
crathira7687 <crathira2002@gmail.com>
2026-08-04 23:35:42 +0530

    practice5

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1/practice5.py
@@ -0,0 +1,6 @@
+#converting one datatype to another(string to integer)
+current_age="21"
+current_age=int(current_age)
+next_age=current_age+1
+#displaying the output
+print(f"Age next year:{next_age}")
