7f21e5daeb702b91b8230e28518aa811ef0135d5
crathira7687 <crathira2002@gmail.com>
2026-08-04 22:45:12 +0530

    practice3

---
+++ track/weeks/week-01-language-bridge/Module2-introduction to Python and basic constructs/practice3.py
@@ -0,0 +1,12 @@
+#creating required variables
+student_name="Aarav"
+course_duration=30
+course_rating=4.8
+is_enrolled=True
+
+#Displaying the variables datatype
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
+
