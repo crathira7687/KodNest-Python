6441fb05b778a56b1c62674d06761597082266fa
crathira7687 <crathira2002@gmail.com>
2026-08-04 23:30:38 +0530

    practice4

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1/practice4.py
@@ -0,0 +1,15 @@
+age_text="21"
+course_fee_text="499.50"
+attempts=3
+#convert the values here
+age_text=int(age_text)
+course_fee_text=float(course_fee_text)
+attempts=str(attempts)
+attempts_text=attempts
+#displaying the variables and their types
+print(age_text)
+print(type(age_text))
+print(course_fee_text)
+print(type(course_fee_text))
+print(attempts)
+print(type(attempts))
