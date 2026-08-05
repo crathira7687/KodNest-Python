05cdb9d43408f41b9097d2565cbf4e30edfed23d
crathira7687 <crathira2002@gmail.com>
2026-08-05 21:19:59 +0530

    practice7

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1/practice7.py
@@ -0,0 +1,16 @@
+#create variables
+age_text="21"
+course_fee_text="499.50"
+attempts=3
+#conversions
+age=int(age_text)
+course_fee=float(course_fee_text)
+attempts_text=str(attempts)
+#display each converted value followed by its  datatype
+print(age)
+print(type(age))
+print(course_fee)
+print(type(course_fee))
+print(attempts_text)
+print(type(attempts_text))
+
