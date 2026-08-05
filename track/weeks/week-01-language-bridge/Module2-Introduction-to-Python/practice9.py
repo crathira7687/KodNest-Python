6ef80efd3a4a03c9c5982cdef55190e2cc7fa7b7
crathira7687 <crathira2002@gmail.com>
2026-08-05 23:53:47 +0530

    practice9

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/practice9.py
@@ -0,0 +1,12 @@
+student_name=input("Enter the name:")
+student_age=int(input("Enter the age:"))
+course_rating=float(input("Enter the rating:"))
+#diaplay the entered values
+print(f"Student:{student_name}")
+print(f"Age:{student_age}")
+print(f"Rating:{course_rating}")
+#display datatypes
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
+
