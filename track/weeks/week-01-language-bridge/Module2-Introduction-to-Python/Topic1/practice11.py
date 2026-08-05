1a7503d6b930e30df5a4241c093e3319186c7b0a
crathira7687 <crathira2002@gmail.com>
2026-08-06 00:50:16 +0530

    practice11

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1/practice11.py
@@ -0,0 +1,6 @@
+first_number=int(input("enter the first number:"))
+second_number=int(input("enter the second number:"))
+#calculate sum
+sum=first_number+second_number
+#display result
+print(f"Sum:{sum}")
