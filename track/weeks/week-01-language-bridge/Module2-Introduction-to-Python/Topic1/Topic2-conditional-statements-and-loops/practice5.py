e75d5ddfdfabdf2df69540a13d1830d2ef10cb4b
crathira7687 <crathira2002@gmail.com>
2026-08-07 23:31:33 +0530

    forloop

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1/Topic2-conditional-statements-and-loops/practice5.py
@@ -0,0 +1,9 @@
+N=int(input("enter a number"))
+word=input("enter a word")
+#use for loop
+print("Numbers:")
+for num in range(N):
+    print(num)
+print("Characters:")
+for char in word:
+    print(char)
