c081e303035df85cc97733395616cb4eaf9e8bb4
crathira7687 <crathira2002@gmail.com>
2026-08-08 16:46:39 +0530

    listoperations

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1-intoduction-to-python-and-basic-constructs/Topic3-Lists,tuples,strings,slicing,mutability/practice2.py
@@ -0,0 +1,16 @@
+n=int(input("enter score count:"))
+scores=[]
+
+for i in range(n):
+    score=int(input("enter the score:"))
+    scores.append(score)
+search_for=int(input("enter a number:"))
+
+
+print("Highest Score:",max(scores))
+print("Lowest Score:",min(scores))
+print("Total Score:",sum(scores))
+if search_for in scores:
+    print("Search Result:Found")
+else:
+    print("Search Result:Not Found")
