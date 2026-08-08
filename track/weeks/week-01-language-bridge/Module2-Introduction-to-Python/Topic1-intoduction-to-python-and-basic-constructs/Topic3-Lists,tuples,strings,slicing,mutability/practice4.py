a59dd8115038a77b226340d67b799497f7bf2512
crathira7687 <crathira2002@gmail.com>
2026-08-08 18:44:27 +0530

    tuplepgm

---
+++ track/weeks/week-01-language-bridge/Module2-Introduction-to-Python/Topic1-intoduction-to-python-and-basic-constructs/Topic3-Lists,tuples,strings,slicing,mutability/practice4.py
@@ -0,0 +1,14 @@
+course_name=input("enter the course name:")
+current_week=input("enter the current week:")
+course_status=input("enter the course status:")
+
+
+course_details=(course_name,current_week,course_status)
+
+
+updated_week=input("enter the updated week:")
+
+course_details=(course_name,updated_week,course_status)
+
+print(course_details)
+
